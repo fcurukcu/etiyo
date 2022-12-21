d5f7691c267204d6b2303d317e47e9d0a160df80
fcurukcu <filiz.curukcu@izmirekonomi.edu.tr>
2022-12-21 17:29:13 +0300

    mail box başlangıç

--- frontend/src/SuperAdmin/Components/Mailbox/Mailbox.js
+++ frontend/src/SuperAdmin/Components/Mailbox/Mailbox.js
@@ -1,22 +1,81 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
-
+import './Mailbox.css';
 class Mailbox extends Component {
   render() {
     return (
     <>
       <div className='row'>
         <div className='col-md-3'>
-          <h1>Mailbox</h1>
-          <Link to='/view' className='btn btn-success btn-sm'><i className='fa fa-plus'></i> Compose</Link>
-          <span>MailBox</span>
-          <ul>
-            <li>
-            <i className='fa fa-plus'></i> Inbox
-            </li>
+          <div className='row mailBoxRow'>
+          <h1 className='mailBoxH1'>Mailbox</h1>
+          <Link to='/view' className='btn btn-success btn-sm mailBoxLink'><i className='fa fa-plus'></i> Compose</Link>
+          
+          </div>
+          <span className='mailBoxSpan'>MailBoxes</span>
+          <ul className='mailBoxUl'>
+            <Link className='mailBoxUlLi activeLi'><i className='fa fa-inbox mailBoxUlLii'></i> 
+            <div className='mailBoxUldiv'>
+              <span className='mailBoxUlDivSpan'>
+                Inbox
+              </span>
+            </div></Link>
+
+            <Link className='mailBoxUlLi'><i className='fa fa-paper-plane mailBoxUlLii'></i> 
+            <div className='mailBoxUldiv'>
+              <span className='mailBoxUlDivSpan'>
+                Sent
+              </span>
+            </div></Link>
+
+            <Link className='mailBoxUlLi'><i className='fa fa-file mailBoxUlLii'></i> 
+            <div className='mailBoxUldiv'>
+              <span className='mailBoxUlDivSpan'>
+                Drafts
+              </span>
+            </div></Link>
+
+            <Link className='mailBoxUlLi'><i className='fa fa-exclamation-triangle mailBoxUlLii'></i> 
+            <div className='mailBoxUldiv'>
+              <span className='mailBoxUlDivSpan'>
+                Span
+              </span>
+            </div></Link>
+
+            <Link className='mailBoxUlLi'><i className='fa fa-trash mailBoxUlLii'></i> 
+            <div className='mailBoxUldiv'>
+              <span className='mailBoxUlDivSpan'>
+                Trash
+              </span>
+            </div></Link>
+            
           </ul>
         </div>
-        <div className='col-md-4'></div>
+        <div className='col-md-4'>
+          <div className='message'>
+          <div className='messageDiv'>
+            <div className='messageFlex'>
+                <div className='messageSticky'>
+                    <div className='messafeFlexS'>
+                      <div className='messafeFlexSDiv'>
+                        <p className='messafeFlexSDivP'>Inbox</p>
+                      </div>
+                      <div className='messafeFlexSearch'>
+                          <div className='messafeFlexSearchIcon'>
+                            <i className='fa fa-search mfsfsSearch'></i>
+                          </div>
+                          <input placeholder="Search for an e-mail or task" type="text" aria-label="Search"
+                           class="mfsfsInput" value=""></input>
+                      </div>
+                    </div>
+                </div>
+                <div>
+
+                </div>
+            </div>
+          </div>
+          </div>
+        </div>
         <div className='col-md-5'></div>
       </div></>
        )
