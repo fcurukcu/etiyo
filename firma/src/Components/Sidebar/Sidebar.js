1f17e3120808ca3b856448c55866d5eb992c3e37
Şeymanur Çürükcü <seymanurcurukcu@gmail.com>
2022-12-25 14:44:35 +0300

    logolar eklendi

--- firma/src/Components/Sidebar/Sidebar.js
+++ firma/src/Components/Sidebar/Sidebar.js
@@ -26,7 +26,7 @@ const Sidebar = ({page}) => {
 
             <div className='nav-listadmin'>
             <Link to='/admin' className='nav-linkadmin active'>
-              <i className={`fas fa-th-large nav-link-icon ${show ? 'nav-link-icon-show' : null}`}></i>
+              <i className={`fas fa-home nav-link-icon ${show ? 'nav-link-icon-show' : null}`}></i>
               <span className='nav-link-name'>Anasayfa</span>
             </Link>
             <Link to='/admin/templates' className='nav-linkadmin active'>
@@ -34,7 +34,7 @@ const Sidebar = ({page}) => {
               <span className='nav-link-name'>Temalar</span>
             </Link>
             <Link to='/admin/orders' className='nav-linkadmin'>
-              <i className={`fa-solid fa-bag-shopping  nav-link-icon ${show ? 'nav-link-icon-show' : null}`}></i>
+              <i className={`fa-solid fa-shopping-basket  nav-link-icon ${show ? 'nav-link-icon-show' : null}`}></i>
                 <span className='nav-link-name'>Siparişler</span>
               </Link>
               <Link to='/admin/products' className='nav-linkadmin'>
@@ -42,20 +42,20 @@ const Sidebar = ({page}) => {
                 <span className='nav-link-name'>Ürünler</span>
               </Link>
               <Link to='/admin/categories' className='nav-linkadmin'>
-              <i className={`fa-solid fa-bag-shopping  nav-link-icon ${show ? 'nav-link-icon-show' : null}`}></i>
+              <i className={`fa-solid fa-columns  nav-link-icon ${show ? 'nav-link-icon-show' : null}`}></i>
                 <span className='nav-link-name'>Kategoriler</span>
               </Link>
               <Link to='/admin/settings' className='nav-linkadmin'>
-              <i className={`fa-solid fa-bag-shopping  nav-link-icon ${show ? 'nav-link-icon-show' : null}`}></i>
+              <i className={`fa-solid fa-cog  nav-link-icon ${show ? 'nav-link-icon-show' : null}`}></i>
                 <span className='nav-link-name'>Ayarlar</span>
               </Link>
               <Link to='/admin/web-site' className='nav-linkadmin'>
-              <i className={`fa-solid fa-bag-shopping  nav-link-icon ${show ? 'nav-link-icon-show' : null}`}></i>
+              <i className={`fa-solid fa-sitemap  nav-link-icon ${show ? 'nav-link-icon-show' : null}`}></i>
                 <span className='nav-link-name'>Web Sitem</span>
               </Link>
 
               <Link to='/admin/support' className='nav-linkadmin'>
-              <i className={`fa-solid fa-bag-shopping  nav-link-icon ${show ? 'nav-link-icon-show' : null}`}></i>
+              <i className={`fa-solid fa-question-circle   nav-link-icon ${show ? 'nav-link-icon-show' : null}`}></i>
                 <span className='nav-link-name'>Destek</span>
               </Link>
              
